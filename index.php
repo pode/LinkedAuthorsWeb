fd2170c63c97414889fc7ff14c031013e7509301
Magnus Enger <magnus@torfeus.deich.folkebibl.no>
2010-10-06 11:12:17 +0200

    Issues 8, 11, 12, 17

--- index.php
+++ index.php
@@ -35,6 +35,24 @@ if (!empty($_GET['lexvo']) && !empty($_GET['label'])) {
 	$url = '';
 	if ($_GET['what'] == 'dbpedia') {
 		// urlencode($_GET['who'])
+		
+		$sparql = '
+SELECT DISTINCT * WHERE {
+<' . $_GET['who'] . '> dbpedia-owl:thumbnail ?thumbnail 
+OPTIONAL { <' . $_GET['who'] . '> dbpedia-owl:activeYearsEndYear ?activeYearsEndYear } .
+OPTIONAL { <' . $_GET['who'] . '> dbpedia-owl:activeYearsStartYear ?activeYearsStartYear } .
+OPTIONAL { <' . $_GET['who'] . '> dbpedia-owl:birthDate ?birthDate } .
+OPTIONAL { <' . $_GET['who'] . '> dbpedia-owl:birthName ?birthName } .
+OPTIONAL { <' . $_GET['who'] . '> dbpedia-owl:birthPlace ?birthPlace } .
+OPTIONAL { ?birthPlace dbpprop:name ?birthPlaceName } .
+OPTIONAL { <' . $_GET['who'] . '> dbpedia-owl:deathDate ?deathDate } .
+OPTIONAL { <' . $_GET['who'] . '> dbpedia-owl:deathPlace ?deathPlace } .
+OPTIONAL { ?deathPlace dbpprop:name ?deathPlaceName } .
+OPTIONAL { <' . $_GET['who'] . '> dbpedia-owl:nationality ?nationality } .
+OPTIONAL { ?nationality dbpedia-owl:thumbnail ?nationalityThumbnail } .
+OPTIONAL { ?nationality dbpprop:nativeName ?nationalityLabel } .
+}';
+		
 /* Example: 
 SELECT DISTINCT * WHERE {
 <' . urlencode($_GET['who']) . '> dbpedia-owl:thumbnail ?thumbnail 
@@ -48,7 +66,7 @@ OPTIONAL { <' . urlencode($_GET['who']) . '> dbpedia-owl:deathPlace ?deathPlace
 OPTIONAL { <' . urlencode($_GET['who']) . '> dbpedia-owl:nationality ?nationality } .
 }
 */
-		$url = 'http://dbpedia.org/sparql?default-graph-uri=http://dbpedia.org&should-sponge=&query=SELECT+DISTINCT+*+WHERE+{%0D%0A%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:thumbnail+%3Fthumbnail+.%0D%0AOPTIONAL+{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:activeYearsEndYear+%3FactiveYearsEndYear+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:activeYearsStartYear+%3FactiveYearsStartYear+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:birthDate+%3FbirthDate+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:birthName+%3FbirthName+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:birthPlace+%3FbirthPlace+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:deathDate+%3FdeathDate+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:deathPlace+%3FdeathPlace+}+.+%0D%0A}&format=' . urlencode('application/sparql-results+json');
+		$url = 'http://dbpedia.org/sparql?default-graph-uri=http://dbpedia.org&should-sponge=&query=' . urlencode($sparql) . '&format=' . urlencode('application/sparql-results+json');
 		// w/nationality $url = 'http://dbpedia.org/sparql?default-graph-uri=http://dbpedia.org&should-sponge=&query=SELECT+DISTINCT+*+WHERE+{%0D%0A%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:thumbnail+%3Fthumbnail+.%0D%0AOPTIONAL+{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:activeYearsEndYear+%3FactiveYearsEndYear+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:activeYearsStartYear+%3FactiveYearsStartYear+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:birthDate+%3FbirthDate+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:birthName+%3FbirthName+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:nationality+%3Fnationality+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:birthPlace+%3FbirthPlace+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:deathDate+%3FdeathDate+}+.+%0D%0AOPTIONAL++{+%3C' . urlencode($_GET['who']) . '%3E+dbpedia-owl:deathPlace+%3FdeathPlace+}+.+%0D%0A}&format=' . urlencode('application/sparql-results+json');
 	
 	} elseif($_GET['what'] == 'dbpedianame') {
@@ -86,52 +104,62 @@ echo('<?xml version="1.0" encoding="utf-8" ?>');
 <script src="jquery/jquery-1.4.2.min.js" type="text/javascript"></script>
 <script type="text/javascript">
 
-// Global variables to hold languages and formats
+var endpointprefix = 'http://bibpode.no/rdfstore/endpoint.php?query=';
+var endpointpostfix = '&jsonp=?';
+
 var languages = new Array();
-
-var formats = new Array();
-// Pre-fetch the formats
-/* SPARQL: 
-PREFIX pode: <http://www.bibpode.no/vocabulary#>
-PREFIX rdf :<http://www.w3.org/1999/02/22-rdf-syntax-ns#>
-PREFIX rdfs: <http://www.w3.org/2000/01/rdf-schema#>
-SELECT DISTINCT * WHERE {
-?format a pode:PhysicalFormat .
-?format rdfs:label ?label .
-}
-*/
-var url = 'http://bibpode.no/rdfstore/endpoint.php?query=PREFIX+pode%3A+%3Chttp%3A%2F%2Fwww.bibpode.no%2Fvocabulary%23%3E%0D%0APREFIX+rdf+%3A%3Chttp%3A%2F%2Fwww.w3.org%2F1999%2F02%2F22-rdf-syntax-ns%23%3E%0D%0APREFIX+rdfs%3A+%3Chttp%3A%2F%2Fwww.w3.org%2F2000%2F01%2Frdf-schema%23%3E%0D%0A%0D%0ASELECT+DISTINCT+*+WHERE+{%0D%0A%3Fformat+a+pode%3APhysicalFormat+.%0D%0A%3Fformat+rdfs%3Alabel+%3Flabel+.%0D%0A}&jsonp=?';
-var params = { 'output': 'json' };
-
-$.getJSON(url, params, function(json, status) {
-	if (json.results.bindings){
-		// alert(json.results.bindings[0].title.value);
-		$.each(json.results.bindings, function(i, n) {
-			var item = json.results.bindings[i];
-			formats[item.format.value] = item.label.value;
-		});
-	} else {
-		alert('Unable to fetch formats');
-	}
-});
-
 
 $(document).ready(function() {
 
 });
+
+var author = '';
 
 function showWorks() {
 
 	// Clear the list of works
 	$('#workslist').empty();
 	
-	var author = '';
-	if ($("#authorselect option:selected").val() != '') {
+	// alert('author: ' + author);
+	if ($("#authorselect option:selected").val() != '' && $("#authorselect option:selected").val() != author) {
+		
+		// alert('ny forfatter! : ' + $("#authorselect option:selected").val());
 		author = $("#authorselect option:selected").val();
-		showAuthor($("#authorselect option:selected").val());
-	} else {
-		return;
-	}
+		showAuthor(author);
+		
+		// Display the list of available languages for this author
+					  var language_sparql = 'PREFIX dct: <http://purl.org/dc/terms/> \n';
+		language_sparql = language_sparql + 'PREFIX frbr: <http://purl.org/vocab/frbr/core#> \n';
+		language_sparql = language_sparql + 'PREFIX person: <http://www.bibpode.no/person/> \n';
+		language_sparql = language_sparql + 'SELECT DISTINCT ?language WHERE { \n';
+		language_sparql = language_sparql + '?work a frbr:Work ; \n';
+		language_sparql = language_sparql + 'dct:title ?title ; \n';
+		language_sparql = language_sparql + 'dct:creator ' + author + ' . \n';
+		language_sparql = language_sparql + '?expression frbr:realizationOf ?work . \n';
+		language_sparql = language_sparql + '?expression dct:language ?language . \n';
+		language_sparql = language_sparql + '}';
+		var language_url = endpointprefix + escape(language_sparql) + endpointpostfix;
+		var params = { 'output': 'json' };
+		// Empty the list
+		$('#language_select').empty();
+		$('#language_select').append('<option value="">Velg språk...</option>');
+		// Get the actual data
+		$.getJSON(language_url, params, function(json, status) {
+			if (json.results.bindings){
+				$.each(json.results.bindings, function(i, n) {
+					var item = json.results.bindings[i];
+					if (languages[item.language.value]) {
+						$('#language_select').append('<option value="' + item.language.value + '">' + languages[item.language.value] + '</option>');
+					} else {
+						$('#language_select').append('<option value="' + item.language.value + '">' + item.language.value + '</option>');
+					}
+				});
+			} else {
+				alert('Something went wrong...');
+			}
+		});
+		
+	} 
 	var sortby = 'firstEdition';
 	if ($("#sortby option:selected").val() == 'title') {
 		sortby = 'title';
@@ -141,26 +169,32 @@ function showWorks() {
 		sortorder = 'DESC';
 	}
 	
-/* Example: 
-PREFIX pode: <http://www.bibpode.no/vocabulary#>
-PREFIX dct: <http://purl.org/dc/terms/>
-PREFIX frbr: <http://purl.org/vocab/frbr/core#>
-PREFIX person: <http://www.bibpode.no/person/>
-
-SELECT DISTINCT ?work ?firstEdition ?title WHERE {
-?work a frbr:Work ;
-pode:firstEdition ?firstEdition ;
-dct:title ?title ;
-dct:creator person:Petterson_Per .
-OPTIONAL { ?host frbr:part ?work } .
-FILTER (!bound(?host)) 
-} ORDER BY ASC (?work)
-*/
+	// Show works
+			   var works_sparql = 'PREFIX pode: <http://www.bibpode.no/vocabulary#> \n';
+	works_sparql = works_sparql + 'PREFIX dct: <http://purl.org/dc/terms/> \n';
+	works_sparql = works_sparql + 'PREFIX frbr: <http://purl.org/vocab/frbr/core#> \n';
+	works_sparql = works_sparql + 'PREFIX person: <http://www.bibpode.no/person/> \n';
+	works_sparql = works_sparql + 'SELECT DISTINCT ?work ?firstEdition ?title WHERE { \n';
+	works_sparql = works_sparql + '?work a frbr:Work ; \n';
+	works_sparql = works_sparql + 'pode:firstEdition ?firstEdition ; \n';
+	works_sparql = works_sparql + 'dct:title ?title ; \n';
+	works_sparql = works_sparql + 'dct:creator ' + author + ' . \n';
+	// Check if a particular language has been chosen
+	// alert('språk valgt ' + $("#language_select option:selected").val());
+	if ($("#language_select option:selected").val() != '') {
+		// alert('språk valgt ' + $("#language_select option:selected").val());
+		works_sparql = works_sparql + '?expression a frbr:Expression ; \n';
+		works_sparql = works_sparql + 'frbr:realizationOf ?work ; \n';
+		works_sparql = works_sparql + 'dct:language <' + $("#language_select option:selected").val() + '> . \n';
+	}
+	works_sparql = works_sparql + 'OPTIONAL { ?host frbr:part ?work } . \n';
+	works_sparql = works_sparql + 'FILTER (!bound(?host))  \n';
+	works_sparql = works_sparql + '} ORDER BY ' + sortorder + ' (?' + sortby + ') \n';
 	
-	var url = 'http://bibpode.no/rdfstore/endpoint.php?query=PREFIX+pode%3A+%3Chttp%3A%2F%2Fwww.bibpode.no%2Fvocabulary%23%3E%0D%0APREFIX+dct%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0D%0APREFIX+frbr%3A+%3Chttp%3A%2F%2Fpurl.org%2Fvocab%2Ffrbr%2Fcore%23%3E%0D%0APREFIX+person%3A+%3Chttp%3A%2F%2Fwww.bibpode.no%2Fperson%2F%3E%0D%0A%0D%0ASELECT+DISTINCT+%3Fwork+%3FfirstEdition+%3Ftitle+WHERE+{%0D%0A%3Fwork+a+frbr%3AWork+%3B%0D%0Apode%3AfirstEdition+%3FfirstEdition+%3B%0D%0Adct%3Atitle+%3Ftitle+%3B%0D%0Adct%3Acreator+' + author + '+.%0D%0AOPTIONAL+{+%3Fhost+frbr%3Apart+%3Fwork+}+.%0D%0AFILTER+%28!bound%28%3Fhost%29%29+%0D%0A}+ORDER+BY+' + sortorder + '+%28%3F' + sortby + '%29&jsonp=?';
+	var works_url = endpointprefix + escape(works_sparql) + endpointpostfix;
 	var params = { 'output': 'json' };
 
-	$.getJSON(url, params, function(json, status) {
+	$.getJSON(works_url, params, function(json, status) {
 		if (json.results.bindings){
 			// alert(json.results.bindings[0].title.value);
 			$.each(json.results.bindings, function(i, n) {
@@ -192,31 +226,68 @@ function showExpressions(elemid, workuri) {
 	} else {
 
 		// Show expressions
-		var url='http://bibpode.no/rdfstore/endpoint.php?query=PREFIX+pode%3A+%3Chttp%3A%2F%2Fwww.bibpode.no%2Fvocabulary%23%3E%0D%0APREFIX+pode_lf%3A+%3Chttp%3A%2F%2Fwww.bibpode.no%2Flf%2F%3E+%0D%0APREFIX+dct%3A+%3Chttp%3A%2F%2Fpurl.org%2Fdc%2Fterms%2F%3E%0D%0APREFIX+frbr%3A+%3Chttp%3A%2F%2Fpurl.org%2Fvocab%2Ffrbr%2Fcore%23%3E%0D%0APREFIX+person%3A+%3Chttp%3A%2F%2Fwww.bibpode.no%2Fperson%2F%3E+%0D%0APREFIX+work%3A+%3Chttp%3A%2F%2Fwww.bibpode.no%2Fwork%2F%3E%0D%0A%0D%0ASELECT+DISTINCT+%3Flanguage+%3Fformat+WHERE+{%0D%0A%3Fexpression+a+frbr%3AExpression+.%0D%0A%3Fexpression+frbr%3ArealizationOf+%3C' + workuri + '%3E+.%0D%0A%3Fexpression+dct%3Alanguage+%3Flanguage+.%0D%0A%3Fexpression+dct%3Aformat+%3Fformat+.%0D%0A}%0D%0AORDER+BY+%3Flanguage&jsonp=?'
+			  var expr_sparql = 'PREFIX pode: <http://www.bibpode.no/vocabulary#> \n';
+		expr_sparql = expr_sparql + 'PREFIX pode_lf: <http://www.bibpode.no/lf/> \n';
+		expr_sparql = expr_sparql + 'PREFIX dct: <http://purl.org/dc/terms/> \n';
+		expr_sparql = expr_sparql + 'PREFIX frbr: <http://purl.org/vocab/frbr/core#> \n';
+		expr_sparql = expr_sparql + 'PREFIX person: <http://www.bibpode.no/person/> \n';
+		expr_sparql = expr_sparql + 'PREFIX work: <http://www.bibpode.no/work/> \n';
+		expr_sparql = expr_sparql + 'SELECT DISTINCT * WHERE { \n';
+		expr_sparql = expr_sparql + '?expression a frbr:Expression . \n';
+		expr_sparql = expr_sparql + '?expression frbr:realizationOf <' + workuri + '> . \n';
+		if ($("#language_select option:selected").val() != '') {
+			expr_sparql = expr_sparql + '?expression dct:language <' + $("#language_select option:selected").val() + '> . \n';
+		} else {
+			expr_sparql = expr_sparql + '?expression dct:language ?language . \n';
+		}
+		expr_sparql = expr_sparql + '?expression dct:format ?format . \n';
+		expr_sparql = expr_sparql + '?format rdfs:label ?formatlabel . \n';
+		expr_sparql = expr_sparql + '} \n';
 		
+		var expr_url = endpointprefix + escape(expr_sparql) + endpointpostfix;
 		var params = { 'output': 'json' };
 	
-		$.getJSON(url, params, function(json, status) {
+		$.getJSON(expr_url, params, function(json, status) {
 			if (json.results.bindings){
-				// alert(json.results.bindings[0].title.value);
 				var out = '<ul>';
+				var expressions = new Array();
 				$.each(json.results.bindings, function(i, n) {
 					var item = json.results.bindings[i];
+					if (expressions[item.expression.value]) {
+						// Add the formatlabel to the existing formatlabel
+						expressions[item.expression.value].formatlabel.value = expressions[item.expression.value].formatlabel.value + ', ' + item.formatlabel.value;
+						// If format has been sasved as http://www.bibpode.no/ff/di, replace it with a new URI
+						if (expressions[item.expression.value].format.value && expressions[item.expression.value].format.value == 'http://www.bibpode.no/ff/di') {
+							expressions[item.expression.value].format.value = item.format.value;
+						}
+					} else {
+						expressions[item.expression.value] = item;
+					} 
+				});
+				var i = 0;
+				for (var e in expressions) {
+					// alert(e);
+					var item = expressions[e];
 					var idattribute = elemid + 'expression' + i;
-					out = out + '<li><span id="' + idattribute + '" class="expression" onClick="showManifestations(\'' + idattribute + '\', \'' + workuri + '\', \'' + item.language.value + '\', \'' + item.format.value + '\');">';
-					if (formats[item.format.value]) {
-						out = out + '<span class="format">' + formats[item.format.value] + '</span>';
+					out = out + '<li><span id="' + idattribute + '" class="expression" onClick="showManifestations(\'' + idattribute + '\', \'' + workuri + '\', \'';
+					if (item.language) {
+						out = out + item.language.value;
 					} else {
-						out = out + '<span class="format uri">' + item.format.value + '</span>';
+						out = out + $("#language_select option:selected").val();
 					}
-					out = out + ' på ';
-					if (languages[item.language.value]) {
-						out = out + '<span class="language">' + languages[item.language.value] + '</span>';
-					} else {
-						out = out + '<span class="language languageuri" id="' + idattribute + 'language">' + item.language.value + '</span>';
+					out = out + '\', \'' + item.format.value + '\');">';
+					out = out + '<span class="format">' + item.formatlabel.value + '</span>';
+					if (item.language) {
+						out = out + ' på ';
+						if (languages[item.language.value]) {
+							out = out + '<span class="language">' + languages[item.language.value] + '</span>';
+						} else {
+							out = out + '<span class="language languageuri" id="' + idattribute + 'language">' + item.language.value + '</span>';
+						}
 					}
 					out = out + '</span></li>';
-				});
+					i++;
+				}
 				var out = out + '</ul>';
 				// Hide all "open" works
 				$('.work').siblings().hide();
@@ -278,7 +349,7 @@ function setLanguageLabels(id, uri){
 	var name = '';
 	$.getJSON(url, function(json){
 		// alert(json.label);
-		if (json.label){
+		if (json && json.label){
 			name = json.label;
 		} else {
 			// Return the URI if a name was not found
@@ -287,7 +358,7 @@ function setLanguageLabels(id, uri){
 		}
 		// Save the label for later re-use
 		languages[uri] = name;
-		$('#' + id).empty().append(name).removeClass('uri');
+		$('#' + id).empty().append(name).removeClass('languageuri');
  	});
 
 }
@@ -398,18 +469,27 @@ function getDbpediaData(dbpediaid) {
 					var birthDate = new Date(item.birthDate.value);
 					$('#authorbox').append('<p>Født: ' + birthDate.toLocaleDateString() + '</p>');
 				}
-				if (item.birthPlace) {
-					$('#authorbox').append('<p>Fødselssted: <span id="birthPlace" class="uri">' + item.birthPlace.value + '</span></p>');
+				if (item.birthPlaceName) {
+					$('#authorbox').append('<p>Fødselssted: <span id="birthPlace" class="uri">' + item.birthPlaceName.value + '</span></p>');
+				} else if (item.birthPlace) {
+					var name = dbpediauri2name(item.birthPlace.value);
+					$('#authorbox').append('<p>Fødselssted: <span id="birthPlace" class="uri">' + name + '</span></p>');
 				}
 				if (item.deathDate) {
 					var deathDate = new Date(item.deathDate.value);
 					$('#authorbox').append('<p>Død: ' + deathDate.toLocaleDateString() + '</p>');
 				}
-				if (item.deathPlace) {
-					$('#authorbox').append('<p>Dødssted: <span id="deathPlace" class="uri">' + item.deathPlace.value + '</span></p>');
+				if (item.deathPlaceName) {
+					$('#authorbox').append('<p>Dødssted: <span id="deathPlace">' + item.deathPlaceName.value + '</span></p>');
+				} else if (item.deathPlace) {
+					var name = dbpediauri2name(item.deathPlace.value);
+					$('#authorbox').append('<p>Dødssted: <span id="deathPlace" class="uri">' + name + '</span></p>');
 				}
-				if (item.nationality) {
-					$('#authorbox').append('<p>Nasjonalitet: <span id="deathPlace" class="uri">' + item.nationality.value + '</span></p>');
+				if (item.nationalityThumbnail) {
+					$('#authorbox').append('<p id="nationality">Nasjonalitet:<br /> <img height="72" width="100" src="' + item.nationalityThumbnail.value + '" /></p>');
+					if (item.nationalityLabel) {
+						$('#nationality').append('<br />' + item.nationalityLabel.value);
+					}
 				}
 				if (item.activeYearsStartYear) {
 					var activeYearsStartYear = new Date(item.activeYearsStartYear.value);
@@ -422,11 +502,21 @@ function getDbpediaData(dbpediaid) {
 			});
 		}
  		// Turn URIs into names
- 		$('.uri').each(function(index) {
-    		setDbpediaName($(this).attr('id'), $(this).text());
-		});
+ 		// $('.uri').each(function(index) {
+    	// 	setDbpediaName($(this).attr('id'), $(this).text());
+		// });
  	});
  	
+}
+
+/*
+Sometimes DBpedia only returns a URI for a place, not a name. 
+This function chops off the first 28 cahracters from a URI and returns the reminder in place of a name. 
+Example: 
+http://dbpedia.org/resource/Lom -> Lom
+*/
+function dbpediauri2name(s) {
+	return s.substring(28);
 }
 
 function setDbpediaName(id, uri){
@@ -492,6 +582,10 @@ function setDbpediaName(id, uri){
  <option value="asc"">Velg sortering...</option>
  <option value="asc">Stigende</option>
  <option value="desc">Synkende</option>
+</select>
+<!-- Select language - this list is filled at runtime, based on the author selected -->
+<select name="language_select" id="language_select" onChange="showWorks()">
+ <option value="">Velg språk...</option>
 </select>
 </form>
 
